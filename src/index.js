032d1dc7186b1c854026dc486f80bfb38fd0b7d4
Eric Chan <wschanar@connect.ust.hk>
2020-06-07 14:33:49 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const title = React.createElement('h1', {}, "My First React Code");
+const container = React.createElement('div', {}, title);
 
 ReactDOM.render(
-  title,
+  container,
   document.getElementById('global')
 );
